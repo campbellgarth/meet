d068f802fa0680321302619073824a958d967a1a
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-06-19 18:27:15 +0200

    working and tested feature 1 for meet app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import CitySearch from './components/CitySearch';
+import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <CitySearch />
+      <EventList />
     </div>
   );
- }
- 
- export default App;
+};
+
+export default App;
