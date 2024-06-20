a77bef76d0156c81a124d413363998ed2fcd53c2
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-06-20 02:40:35 +0200

    working tests and code for feature 2

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,6 +1,7 @@
 import Event from '../components/Event';
 import { render } from '@testing-library/react';
 import { getEvents } from '../api';
+import userEvent from '@testing-library/user-event';
 
 describe('<Event /> component', () => {
   let EventComponent;
@@ -28,5 +29,28 @@ describe('<Event /> component', () => {
   });
   test('renders event details button with the title (show details)', () => {
     expect(EventComponent.queryByText('show details')).toBeInTheDocument();
+  });
+  test('by default, event details section should be hidden', () => {
+    expect(
+      EventComponent.container.querySelector('#event-details')
+    ).not.toBeInTheDocument();
+  });
+  test('shows the details section when the user clicks on the "show details" button', async () => {
+    const user = userEvent.setup();
+    const showDetailsButton = EventComponent.getByText('show details');
+    await user.click(showDetailsButton);
+    const eventDetails =
+      EventComponent.container.querySelector('#event-details');
+    expect(eventDetails).toBeInTheDocument();
+  });
+  test('hides the details section when the user clicks on the "hide details" button', async () => {
+    const user = userEvent.setup();
+    const showDetailsButton = EventComponent.getByText('show details');
+    await user.click(showDetailsButton);
+    const hideDetailsButton = EventComponent.getByText('hide details');
+    await user.click(hideDetailsButton);
+    const eventDetails =
+      EventComponent.container.querySelector('#event-details');
+    expect(eventDetails).not.toBeInTheDocument();
   });
 });
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
+
 const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleToggleDetails = () => {
+    //toggle between showing and hiding details button
+    setShowDetails(!showDetails);
+  };
   return (
     <li>
       <h2>{event.summary}</h2>
       <p>{event.location}</p>
       <p>{event.created}</p>
-      <button>show details</button>
+      {showDetails && ( //only show event.description if showDetails is toggled true
+        <div id="event-details">
+          <p>{event.description}</p>
+        </div>
+      )}
+      <button
+        id={showDetails ? 'hide-details' : 'show-details'}
+        onClick={handleToggleDetails}
+      >
+        {showDetails ? 'hide details' : 'show details'}
+      </button>
     </li>
   );
 };
