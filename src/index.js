c9c25a3e9faa8a352a26489d6ab5556485a45b48
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-06-25 16:39:46 +0200

    set up atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('82b44dd00c814944adc4b14f01b6ede8').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
