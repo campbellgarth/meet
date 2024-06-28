f4bb91516dcf3ea021276f7ba1d6bda75ba1f7aa
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-06-28 14:52:11 +0200

    bug fixes check

--- src/api.js
+++ src/api.js
@@ -50,7 +50,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done(); //safe fetched events data to localStorage for offline usage
+      NProgress.done();
       localStorage.setItem('lastEvents', JSON.stringify(result.events));
       return result.events;
     } else return null;
