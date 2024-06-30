4f0acf35c65e3e59ad1d1f8da449ea1e1a775b09
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-06-30 20:32:29 +0200

    attempting to fix bug as piechart not showing on mobile

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -4,9 +4,8 @@ import { ResponsiveContainer, PieChart, Pie } from 'recharts';
 const EventGenresChart = ({ events }) => {
   const [data, setData] = useState([]);
   const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
-
   const getData = () => {
-    return genres.map((genre) => {
+    const data = genres.map((genre) => {
       const filteredEvents = events.filter((event) =>
         event.summary.includes(genre)
       );
@@ -15,12 +14,8 @@ const EventGenresChart = ({ events }) => {
         value: filteredEvents.length,
       };
     });
+    return data;
   };
-
-  useEffect(() => {
-    setData(getData());
-  }, [`${events}`]);
-
   const renderCustomizedLabel = ({
     cx,
     cy,
@@ -30,26 +25,26 @@ const EventGenresChart = ({ events }) => {
     index,
   }) => {
     const RADIAN = Math.PI / 180;
-    const radius = outerRadius * 1.1; // Slightly adjust the position of the labels
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
     return percent ? (
       <text
         x={x}
         y={y}
         fill="#8884d8"
-        className="pie-label"
         textAnchor={x > cx ? 'start' : 'end'}
         dominantBaseline="central"
-        fontSize={window.innerWidth <= 810 ? '10px' : '12px'}
       >
         {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
       </text>
     ) : null;
   };
-
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
   return (
-    <ResponsiveContainer className="responsive-container">
+    <ResponsiveContainer width="99%" height={400}>
       <PieChart>
         <Pie
           data={data}
@@ -57,7 +52,7 @@ const EventGenresChart = ({ events }) => {
           fill="#8884d8"
           labelLine={false}
           label={renderCustomizedLabel}
-          outerRadius={window.innerWidth <= 810 ? 100 : 130}
+          outerRadius={130}
         />
       </PieChart>
     </ResponsiveContainer>
